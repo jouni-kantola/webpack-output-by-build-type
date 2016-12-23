2fef2f566c09959824e7dfb60a4a90382f1f94c7
Jouni Kantola <jouni@kantola.se>
2016-12-23 10:34:20 +0100

    Generated index from template

--- dist/2.2.5235ca2a3b255c9129ab.prod.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([2],{8:function(o,n,e){const s=e(1);o.exports=function(){console.log("module b says 12 + 1 === 13",s(13).thirteen())}}});
--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 const config = require('./config' + (__DEV__ ? '-dev' : ''));
 
 console.dir(config);
-console.log(globalConfig);
 
 require.ensure([], function (require) {
     const a = require('./deps/module-a.js');
-    console.log(a());
+    a();
 });
-
 
 require.ensure([], function (require) {
     const b = require('./deps/module-b.js');
-    console.log(b());
+    b();
 });
 
 require.ensure([], function (require) {
     const c = require('./deps/module-c.js');
-    console.log(c());
+    c();
 });
 
-if (config.debug) {
+
+if (config.debug) {    
     require.ensure([], function (require) {
         const debug = require('./deps/always-debug.js');
-        console.log(debug("i'm accessible by runtime config"));
+        debug("i'm accessible by runtime config");
     }, 'always-debug');
 }
 
 if (__DEV__) {
     require.ensure([], function (require) {
         const debug = require('./deps/dev-debug.js');
-        console.log(debug("i'm accessible in dev build"));
+        debug("i'm accessible in dev build");
     }, 'dev-debug');
 }

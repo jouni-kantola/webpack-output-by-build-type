aa0fd10195d22ab9dba4c4c1fa64e15feffe8074
Jouni Kantola <jouni@kantola.se>
2017-01-16 22:11:37 +0100

    Dependent to non-dependent

--- src/deps/module-a.js
+++ src/deps/module-a.js
@@ -1,3 +1,5 @@
-import is from 'is-thirteen';
+export const a = () => console.log('hello world');
 
-export const a = () => console.log('module a says 12 + 1 === 13', is(12 + 1).thirteen());
+// import is from 'is-thirteen';
+
+// export const a = () => console.log('module a says 12 + 1 === 13', is(12 + 1).thirteen());
